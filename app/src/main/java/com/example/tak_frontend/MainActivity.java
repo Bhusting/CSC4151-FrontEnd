9e09cb27ca0a74f99814084545d0c4956577cb4c
Samuel Wynn <wynns@spu.edu>
2020-04-23 22:14:10 -0700

    Working Bottom Navigation Menu That Selects Fragments

---
+++ app/src/main/java/com/example/tak_frontend/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tak_frontend;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(TaskFragment.newInstance("",""));
+        bottomNavigation.setSelectedItemId(R.id.navigation_task);
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_chore:
+                            openFragment(ChoreFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_leaderboard:
+                            openFragment(LeaderboardFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_task:
+                            openFragment(TaskFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+}
