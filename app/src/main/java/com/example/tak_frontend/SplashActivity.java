ef9db914fc18fd99043b6b77fcfdfa4c414a9ea2
Samuel Wynn <wynns@spu.edu>
2020-04-25 02:28:43 -0700

    Added Beginning Splash Screen

--- app/src/main/java/com/example/tak_frontend/SplashActivity.java
+++ app/src/main/java/com/example/tak_frontend/SplashActivity.java
@@ -2,13 +2,37 @@ package com.example.tak_frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.ActionMode;
+
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_splash);
+
+        Thread splashThread = new Thread(){
+
+            @Override
+            public void run() {
+                try{
+                    super.run();
+                    sleep(10000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        };
+        splashThread.start();
     }
 }
