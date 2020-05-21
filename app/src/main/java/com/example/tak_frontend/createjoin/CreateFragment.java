7faaf5b9c5ab2055662de0ee7b9d706f6eebd355
nollanr1 <nollanr1@spu.edu>
2020-05-20 23:03:14 -0700

    [TF-17] Added buttons to the Create/Join Choice
    The buttons only send information to the logger. I'm unsure of how to pass information between fragments (or switch fragments, for that matter) and I don't want to start doing it willy-nilly as that might leave sizable holes in the security implementation.
    In any case, the comments should make it reasonably clear as to how this fragment is intended to work.

--- app/src/main/java/com/example/tak_frontend/createjoin/CreateFragment.java
+++ app/src/main/java/com/example/tak_frontend/createjoin/CreateFragment.java
@@ -1,4 +1,4 @@
-package com.example.tak_frontend;
+package com.example.tak_frontend.createjoin;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.tak_frontend.R;
 
 
 public class CreateFragment extends Fragment {
