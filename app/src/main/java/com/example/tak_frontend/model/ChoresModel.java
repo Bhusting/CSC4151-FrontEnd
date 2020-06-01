2c5b47d42f6e8a84ed2a35a15377ac0ae42a01b8
rue hyder <ruehyder@outlook.com>
2020-05-31 17:40:39 -0700

    Updated Chore Model

--- app/src/main/java/com/example/tak_frontend/model/ChoresModel.java
+++ app/src/main/java/com/example/tak_frontend/model/ChoresModel.java
@@ -5,6 +5,7 @@ public class ChoresModel {
         public String choreId;
         public String choreName;
         public String completionTime;
+        public String completionDate;
         public String houseId;
         public int  choreTypeId;
 }
